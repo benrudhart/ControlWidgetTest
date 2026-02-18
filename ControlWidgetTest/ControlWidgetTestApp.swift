9ef991beee1a9be56b3255e1c9724eff3cefd997
Ben Rudhart <hi@benrudhart.com>
2026-02-18 09:39:51 +0100

    Initial Commit

---
+++ ControlWidgetTest/ControlWidgetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlWidgetTestApp.swift
+//  ControlWidgetTest
+//
+//  Created by Ben Rudhart on 18.02.26.
+//
+
+import SwiftUI
+
+@main
+struct ControlWidgetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
